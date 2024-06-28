f344cd7eab6c93f079cc6b20851f463edbf3abe5
Eufrasio Quixindo <eufrasio.dev@gmail.com>
2024-06-29 00:35:02 +0100

    Updating everything

--- src/com/quixindo/study/Arr.java
+++ src/com/quixindo/study/Arr.java
@@ -1,4 +1,33 @@
 package com.quixindo.study;
 
+/*
+Today i'm learning about arrays
+Arrays are used to store multiple values in a single variable
+*/
 public class Arr {
+    public static void main(String[] args) {
+        /*
+        String[] cars = {"Camaro, Corvette, Tesla"};
+
+        cars[0] = "BMW";
+        System.out.println(cars[0]);
+        */
+
+        String[] cars = new String[3];
+
+        cars[0] = "Camaro";
+        cars[1] = "Corvette";
+        cars[2] = "Tesla";
+
+        for (String car : cars) {
+            System.out.println(car);
+        }
+        /*
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+        */
+
+    }
+
 }
